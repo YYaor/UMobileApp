f723bdcc78dd5c2122df4ce726737dfc87f603de
lichangcong <332934863@qq.com>
2015-05-04 23:34:01 +0800

    开单默认界面，storyboard上的自动布局太难用，搞了半天估计还没搞对

---
+++ UMobile/SetDefaultConfigViewController.m
@@ -0,0 +1,37 @@
+//
+//  SetDefaultConfigViewController.m
+//  UMobile
+//
+//  Created by live on 15-5-4.
+//  Copyright (c) 2015年  APPLE. All rights reserved.
+//
+
+#import "SetDefaultConfigViewController.h"
+
+@interface SetDefaultConfigViewController ()
+
+@end
+
+@implementation SetDefaultConfigViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
