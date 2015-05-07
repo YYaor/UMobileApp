f535fcb27965d27f3e52427344801d4ac583b973
李常聪 <lichangcong@yunyao-inc.com>
2015-05-07 13:09:53 +0800

    销售单据查询页面

---
+++ UMobile/YWDJProcutDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  YWDJProcutDetailViewController.m
+//  UMobile
+//
+//  Created by yunyao on 15/5/7.
+//  Copyright (c) 2015年  APPLE. All rights reserved.
+//
+
+#import "YWDJProcutDetailViewController.h"
+
+@interface YWDJProcutDetailViewController ()
+
+@end
+
+@implementation YWDJProcutDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
